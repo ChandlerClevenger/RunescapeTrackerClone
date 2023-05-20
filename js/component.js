0bffbcf5fd4d7051cb0cfc780089dbf40329c536
Chandler Clevenger <Chandler888@icloud.com>
2023-05-20 17:38:34 -0400

    Reformat

--- js/component.js
+++ js/component.js
@@ -1,12 +1,12 @@
-var component = function() {};
+var component = function () {};
 
 /**
  * Render a component.
  *
  * @param {HTMLElement} parent
  */
-component.prototype.render = function(parent) {
-  const container = document.createElement('div');
-  this.decorate(container);
-  parent.appendChild(container);
-}
+component.prototype.render = function (parent) {
+    const container = document.createElement("div");
+    this.decorate(container);
+    parent.appendChild(container);
+};
